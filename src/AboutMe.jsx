b6d4d6e4badf59b4e0f2e96339e549e0d3b926e9
Karl Rodulfo <karlrodulfo@gmail.com>
2019-07-23 22:39:36 -0400

    responsive

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -19,35 +19,62 @@ const AboutMe = (props) => {
     return (
         <div>
             <section className="about-me">
-                <div className="slider-mobile">
-                    <Slider {...navsettings}>
+                {(props.viewModel === 'mobile' || props.viewModel === 'tablet')
+                    ?
+                    <div className="slider-mobile">
+                        <Slider {...navsettings}>
+                            <div className="about-box">
+                                <p>I'm a developer, artist, nerd, and media maven. I want to create elegant, intuitive, web products for companies eager to disrupt their industry. </p>
+                                <p>Prior to software development, I had a career as a television producer, so I've always been pursuing my passion of bringing a vision to life. I thrive on creative problem-solving. </p>
+                            </div>
+                            <div className="about-box">
+                                <div className=" external-link-container">
+                                    <a href="http://www.karlrodulfo.me" target="_blank"><img className="about-image" src="images/tv.jpg" /></a>
+                                    <div>
+                                        <h4><a href="http://www.karlrodulfo.me" target="_blank">TELEVISION</a></h4>
+                                        <p> In my past life, I worked as a television producer. I created and developed original documentary and reality content for Discovery and Animal Planet for several years. See some of my past work here. </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="about-box">
+                                <div className=" external-link-container">
+                                    <a href="http://www.instagram.com/comicswithak" target="_blank"><img className="about-image" src="images/comic.png" /></a>
+                                    <div>
+                                        <h4><a href="http://www.instagram.com/comicswithak" target="_blank">WEBCOMIC</a></h4>
+                                        <p> I'm a hobby cartoonist and I maintain, write, and draw my own comic series in my free time. I launched a webcomic in May 2018, and quickly garnered several thousand followers. Check them out!</p>
+                                    </div>
 
-                        <div className="about-box">
-                            <p>I'm a developer, artist, nerd, and media maven. I want to create elegant, intuitive, web products for companies eager to disrupt their industry. </p>
-                            <p>In my past life, I was a television producer, so even in my previous career, I was bringing a vision to life. I thrive on creative problem-solving. </p>
+                                </div>
+                            </div>
+                        </Slider>
+                    </div>
+                    :
+                    <>
+                        <div className="about-box padded">
+                            <p>I'm a developer, artist, media enthusiast, and nerd. I'm here to create elegant, intuitive, web products for companies eager to disrupt their industry. </p>
+                            <p>Prior to software development, I had a career as a television producer, so I've always been pursuing my passion of bringing a creative vision to life. I thrive on creative problem-solving. </p>
                         </div>
                         <div className="about-box">
                             <div className=" external-link-container">
-                            <a href="http://www.karlrodulfo.me" target="_blank"><img className="about-image" src="images/tv.jpg" /></a>
-                            <div>
-                                <h4><a href="http://www.karlrodulfo.me" target="_blank">TELEVISION</a></h4>
-                                <p> In my past life, I worked as a television producer. I created and developed original documentary and reality content for Discovery and Animal Planet for several years. See some of my past work here. </p>
+                                <a href="http://www.karlrodulfo.me" target="_blank"><img className="about-image" src="images/tv.jpg" /></a>
+                                <div>
+                                    <h4><a href="http://www.karlrodulfo.me" target="_blank">TELEVISION</a></h4>
+                                    <p> In my past life, I worked as a television producer. I created and developed original documentary and reality content for Discovery and Animal Planet for several years. See some of my past work here. </p>
                                 </div>
                             </div>
                         </div>
                         <div className="about-box">
                             <div className=" external-link-container">
-                            <a href="http://www.instagram.com/comicswithak" target="_blank"><img className="about-image" src="images/comic.png" /></a>
-                            <div>
-                                <h4><a href="http://www.instagram.com/comicswithak" target="_blank">WEBCOMIC</a></h4>
-                                <p> I'm a hobby cartoonist and I maintain, write, and draw my own comic series in my free time. I launched a webcomic in May 2018, and quickly garnered several thousand followers. Check them out!</p>
+                                <a href="http://www.instagram.com/comicswithak" target="_blank"><img className="about-image" src="images/comic.png" /></a>
+                                <div>
+                                    <h4><a href="http://www.instagram.com/comicswithak" target="_blank">WEBCOMIC</a></h4>
+                                    <p> I'm a hobby cartoonist and I maintain, write, and draw my own comic series in my free time. I launched a webcomic in May 2018, and quickly garnered several thousand followers. Check them out!</p>
                                 </div>
 
                             </div>
                         </div>
-                    </Slider>
-                </div>
-
+                    </>
+                }
             </section>
 
             <style> {`
@@ -58,7 +85,7 @@ const AboutMe = (props) => {
                     max-width: 800px;
                     background: #397b82;
                     height: 100%;
-                    max-height: 60vh;
+                    max-height: 80vh;
                     margin: auto;
                     margin-top: -3rem;
                     border-radius: .5rem;
@@ -70,6 +97,9 @@ const AboutMe = (props) => {
                 .about-box {
                     padding: 1rem;
                      ${(props.viewModel === 'mobile') ? 'max-height: 50vh' : ''}
+                }
+                .padded {
+                    margin: 0 1rem;
                 }
                 .about-me p {
                     margin: .5rem 0;
@@ -86,14 +116,14 @@ const AboutMe = (props) => {
                     border-bottom: 0;
                     border-left: 0;
                     margin-left: -10px;
-                    margin-bottom: -20px;
+                    margin-bottom: -19px;
                 }
                 .about-me h4 {
                     margin: 0 0 .5rem;
                     color: #bde51a
                 }
                 img.about-image {
-                    width: 150px;
+                    width: 120px;
                     margin: 0 1rem;
                     border-radius: .5rem;
                     box-shadow: 0 0 11px 1px #0000008f
